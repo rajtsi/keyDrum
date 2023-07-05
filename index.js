e13456da2bad39aa5a503da99afcbe5f8cf37db6
Rajat Singh <112238535+rajtsi@users.noreply.github.com>
2023-07-05 14:15:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-//alert("a botton had been clicked");
 
-// function cra()
-//    {var audiosound=new Audio("sounds/crash.mp3");
-//         audiosound.play();}
-
-
-
-
-
-// for(i=0;i<7;i++)
-// { document.querySelectorAll("button")[i]
 document.addEventListener("keydown",function(event)
 {
 
@@ -50,3 +39,47 @@ document.addEventListener("keydown",function(event)
 }
 
 );
+
+
+ for(i=0;i<7;i++)
+ { document.querySelectorAll("button")[i].addEventListener("click",function()
+{
+ 
+   switch (this.innerHTML) {
+    case "w": var audiosound=new Audio("sounds/tom-1.mp3");
+               audiosound.play();
+        
+        break;
+        case "a": var audiosound=new Audio("sounds/tom-2.mp3");
+        audiosound.play();
+ 
+ break;
+ case "s": var audiosound=new Audio("sounds/tom-3.mp3");
+               audiosound.play();
+        
+        break;
+        case "d": var audiosound=new Audio("sounds/tom-4.mp3");
+               audiosound.play();
+        
+        break;
+        case "j": var audiosound=new Audio("sounds/snare.mp3");
+               audiosound.play();
+        
+        break;
+        case "k": var audiosound=new Audio("sounds/kick-bass.mp3");
+               audiosound.play();
+        
+        break;
+        case "l": var audiosound=new Audio("sounds/crash.mp3");
+               audiosound.play();
+        
+        break;    
+   
+    default:
+        break;
+   }
+
+}
+
+);
+ }
